25ec4fcfdc491f5b135d98bda5362e293a3dfc67
Kezia Regina <keziaregina28@gmail.com>
2025-10-09 00:13:49 +0700

    add : test get api

---
+++ app/product/page.tsx
@@ -0,0 +1,22 @@
+import { Product as ProductType } from "@/types/product";
+
+export default async function Product() {
+  const res = await fetch("http://localhost:3000/api/product", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch produk");
+  }
+
+  const products: ProductType[] = await res.json();
+
+  return (
+    <div>
+      <h1>hello world!</h1>
+      {products.map((item) => (
+        <li key={item._id}>{item._id}</li>
+      ))}
+    </div>
+  );
+}    
