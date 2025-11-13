aec97da033243a4a92cb0ce4d1e68a01d4655bc1
Kezia Regina <keziaregina28@gmail.com>
2025-11-13 21:59:50 +0700

    add: init prisma client for global

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-export const prisma = 
-    globalForPrisma.prisma || 
-    new PrismaClient({
-        log: ["query", "info", "warn", "error"],
-    });
+let prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.APP_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
