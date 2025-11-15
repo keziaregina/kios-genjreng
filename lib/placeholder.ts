0ce6ecdc4e548e3c66ea097bac026c47ac43fb1b
Kezia Regina <keziaregina28@gmail.com>
2025-11-15 13:22:30 +0700

    add: introduce font and placeholder data, along with new asset images for categories and popular items

---
+++ lib/placeholder.ts
@@ -0,0 +1,50 @@
+export const categories = [
+  {
+    id: 1,
+    name: "Akustik",
+    image: "/assets/category/akustik.png"
+  },
+  {
+    id: 2,
+    name: "Elektrik",
+    image: "/assets/category/melodi.png"
+  },
+  {
+    id: 3,
+    name: "Bass",
+    image: "/assets/category/bass.png"
+  },
+  {
+    id: 4,
+    name: "Melodi",
+    image: "/assets/category/bass.png"
+  },
+  {
+    id: 5,
+    name: "Ukulele",
+    image: "/assets/category/bass.png"
+  },
+];
+
+export const popularly = [
+  {
+    id: 1,
+    name: "Akustik 123",
+    image: "/assets/terpopuler-guitar1.svg"
+  },
+  {
+    id: 2,
+    name: "Bass 111",
+    image: "/assets/terpopuler-guitar2.svg"
+  },
+  {
+    id: 3,
+    name: "Electricity",
+    image: "/assets/terpopuler-guitar1.svg"
+  },
+  {
+    id: 4,
+    name: "Melody",
+    image: "/assets/terpopuler-guitar2.svg"
+  },
+]
