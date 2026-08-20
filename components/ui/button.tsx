1d1bcdd5ebd5192683a9afb61e9ad4deead4d107
Kezia Regina <keziaregina28@gmail.com>
2026-08-20 15:58:19 +0700

    fix: add an accent button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,7 +19,9 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
-        selected: "text-text-secondary bg-quarternary hover:bg-button-primary hover:text-text-primary"
+        selected: "text-text-secondary bg-quarternary hover:bg-button-primary hover:text-text-primary",
+        accent:
+          "bg-button-accent-primary text-text-primary hover:bg-button-accent-secondary"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
