3f59df02cd6bb5504b61477b8e1fae98df18b472
Kezia Regina <keziaregina28@gmail.com>
2026-08-21 16:12:31 +0700

    feat: add icon-round button variant

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border border-3 border-button-primary text-button-primary bg-transparent shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost:
@@ -30,6 +30,7 @@ const buttonVariants = cva(
         icon: "size-9",
         "icon-sm": "size-8",
         "icon-lg": "size-10",
+        "icon-round": "size-7 rounded-full",
         form: "h-[56px] w-full rounded-xl px-6 text-sm font-semibold",
       },
     },
